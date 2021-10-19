6754ad882933248079eea4cde961f1142409323d
OtkurO <54796318+OtkurO@users.noreply.github.com>
2021-10-19 15:11:21 +0200

    Set movieSchema

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ mongoose
     console.log("Mongoose CONNECTION OPEN!");
   })
   .catch((err) => console.log("CONNECTION ERROR!\n", err));
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
