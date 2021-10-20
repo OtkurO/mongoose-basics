b7c6ce48bfdc8f1b9978e9993fec723f371a193a
OtkurO <54796318+OtkurO@users.noreply.github.com>
2021-10-20 13:40:17 +0200

    Add example insertMany

--- index.js
+++ index.js
@@ -15,9 +15,20 @@ const movieSchema = new mongoose.Schema({
 
 //Create Movie class
 const Movie = mongoose.model("Movie", movieSchema);
-const theShawshankRedemption = new Movie({
-  title: "The Shawshank Redemption",
-  year: 1994,
-  score: 9.3,
-  rating: "R",
+// const theShawshankRedemption = new Movie({
+//   title: "The Shawshank Redemption",
+//   year: 1994,
+//   score: 9.3,
+//   rating: "R",
+// });
+
+Movie.insertMany([
+  { title: "The Shawshank Redemption", year: 1994, score: 9.3, rating: "R" },
+  { title: "Alien", year: 1979, score: 8.1, rating: "R" },
+  { title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG" },
+  { title: "Stand By Me", year: 1986, score: 8.6, rating: "R" },
+  { title: "Moonrise Kingdom", year: 2012, score: 7.3, rating: "PG-13" },
+]).then((data) => {
+  console.log("Inserted many successfully!");
+  console.log(data);
 });
